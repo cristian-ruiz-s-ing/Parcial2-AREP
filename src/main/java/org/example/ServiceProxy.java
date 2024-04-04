7c808ab0f74cd3f23f66857b42d5a3a533f955a3
Cristian Ruiz <cristiansanta13@hotmail.com>
2024-04-03 20:21:16 -0500

    Cambios

--- src/main/java/org/example/ServiceProxy.java
+++ src/main/java/org/example/ServiceProxy.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 
 public class ServiceProxy {
+    static int temp = 0;
 
     public static void main(String[] args) throws IOException {
         port(getPort());
@@ -19,8 +20,15 @@ public class ServiceProxy {
         get("/", (req, res)-> WebClient.getClient());
 
         get("/mathservice/:fun/:value", (req, res) -> {
-            String URLMathServices = "http://localhost:4568/"+req.params("fun")+"/"+req.params("value");
-            URL obj= new URL(URLMathServices);
+            String[] URLMathServices = {"ec2-34-202-166-243.compute-1.amazonaws.com:4568/"+req.params("fun")+"/"+req.params("value"), "ec2-54-88-94-122.compute-1.amazonaws.com:4569/"+req.params("fun")+"/"+req.params("value")};
+            // Round Robin ""
+            URL obj = new URL(URLMathServices[temp]);
+            System.out.println(obj);
+            if (temp == 0){
+                temp = 1;
+            } else{
+                temp =0;
+            }
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
 
